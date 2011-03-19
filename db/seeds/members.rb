81528571f8e8e291e98e5332fa1bcda0eaf6fe8a
n5ken <n5ken@foxmail.com>
2011-03-19 06:37:31 -0700

    no need to create page for member

--- db/seeds/members.rb
+++ db/seeds/members.rb
@@ -4,14 +4,3 @@ User.find(:all).each do |user|
                         :position => (user.plugins.maximum(:position) || -1) +1)
   end
 end
-
-page = Page.create(
-  :title => 'Members',
-  :link_url => '/members',
-  :deletable => false,
-  :position => ((Page.maximum(:position, :conditions => {:parent_id => nil}) || -1)+1),
-  :menu_match => '^/members(\/|\/.+?|)$'
-)
-Page.default_parts.each do |default_page_part|
-  page.parts.create(:title => default_page_part, :body => nil)
-end
