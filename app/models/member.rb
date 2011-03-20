7b440a9506f38629610301e20c46043f7c6cfaa4
n5ken <n5ken@foxmail.com>
2011-03-20 11:09:17 -0700

    update for the typo

--- app/models/member.rb
+++ app/models/member.rb
@@ -4,9 +4,9 @@ class Member < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-	# Virtual attribute for authenticating by either username or email
-	# This is in addition to a real persisted field like 'username'
-	attr_accessor :login
+  # Virtual attribute for authenticating by either username or email
+  # This is in addition to a real persisted field like 'username'
+  attr_accessor :login
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :login
@@ -20,43 +20,43 @@ protected
     where(conditions).where(["username = :value OR email = :value", { :value => login }]).first
   end
 
-	# Attempt to find a user by it's email. If a record is found, send new
-	# password instructions to it. If not user is found, returns a new user
-	# with an email not found error.
-	def self.send_reset_password_instructions(attributes={})
-	 recoverable = find_recoverable_or_initialize_with_errors(reset_password_keys, attributes, :not_found)
-	 recoverable.send_reset_password_instructions if recoverable.persisted?
-	 recoverable
-	end 
+  # Attempt to find a user by it's email. If a record is found, send new
+  # password instructions to it. If not user is found, returns a new user
+  # with an email not found error.
+  def self.send_reset_password_instructions(attributes={})
+   recoverable = find_recoverable_or_initialize_with_errors(reset_password_keys, attributes, :not_found)
+   recoverable.send_reset_password_instructions if recoverable.persisted?
+   recoverable
+  end 
 
-	def self.find_recoverable_or_initialize_with_errors(required_attributes, attributes, error=:invalid)
-	 case_insensitive_keys.each { |k| attributes[k].try(:downcase!) }
+  def self.find_recoverable_or_initialize_with_errors(required_attributes, attributes, error=:invalid)
+   case_insensitive_keys.each { |k| attributes[k].try(:downcase!) }
 
-	 attributes = attributes.slice(*required_attributes)
-	 attributes.delete_if { |key, value| value.blank? }
+   attributes = attributes.slice(*required_attributes)
+   attributes.delete_if { |key, value| value.blank? }
 
-	 if attributes.size == required_attributes.size
-		 if attributes.has_key?(:login)
-		    login = attributes.delete(:login)
-		    record = find_record(login)
-		 else  
-		   record = where(attributes).first
-		 end  
-	 end  
+   if attributes.size == required_attributes.size
+	   if attributes.has_key?(:login)
+	      login = attributes.delete(:login)
+	      record = find_record(login)
+	   else  
+	     record = where(attributes).first
+	   end  
+   end  
 
-	 unless record
-		 record = new
+   unless record
+	   record = new
 
-		 required_attributes.each do |key|
-		   value = attributes[key]
-		   record.send("#{key}=", value)
-		   record.errors.add(key, value.present? ? error : :blank)
-		 end  
-	 end  
-	 record
-	end
+	   required_attributes.each do |key|
+	     value = attributes[key]
+	     record.send("#{key}=", value)
+	     record.errors.add(key, value.present? ? error : :blank)
+	   end  
+   end  
+   record
+  end
 
-	def self.find_record(login)
-	 where(["username = :value OR email = :value", { :value => login }]).first
-	end
+  def self.find_record(login)
+   where(["username = :value OR email = :value", { :value => login }]).first
+  end
 end
