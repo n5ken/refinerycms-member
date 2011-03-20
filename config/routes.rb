de248396674e4655cc0323a24fb0a47ad06a27be
n5ken <n5ken@foxmail.com>
2011-03-20 11:04:00 -0700

    remove the default route for front end

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Refinery::Application.routes.draw do
   devise_for :members
 
-  resources :members, :only => [:index, :show]
-
   scope(:path => 'refinery', :as => 'admin', :module => 'admin') do
     resources :members, :except => :show do
       collection do
