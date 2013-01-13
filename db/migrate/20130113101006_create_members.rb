bace346a76f4570763cea55d093e9938c60f9e98
Slava Kravchenko <slava.kravchenko@gmail.com>
2013-01-13 22:23:23 +0300

    updated create_members migration to Devise 2

--- db/migrate/20130113101006_create_members.rb
+++ db/migrate/20130113101006_create_members.rb
@@ -3,21 +3,24 @@ class CreateMembers < ActiveRecord::Migration
     create_table(:members) do |t|
       t.string :username
 
-      t.database_authenticatable :null => false
-      t.recoverable
-      t.rememberable
-      t.trackable
+      t.string :email,              :null => false, :default => ""
+      t.string :encrypted_password, :null => false, :default => ""
 
-      # t.confirmable
-      # t.lockable :lock_strategy => :failed_attempts, :unlock_strategy => :both
-      # t.token_authenticatable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+
+      t.datetime :remember_created_at
+
+      t.integer  :sign_in_count, :default => 0
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
 
       t.timestamps
     end
 
     add_index :members, :email,                :unique => true
     add_index :members, :reset_password_token, :unique => true
-    # add_index :members, :confirmation_token,   :unique => true
-    # add_index :members, :unlock_token,         :unique => true
   end
 end
