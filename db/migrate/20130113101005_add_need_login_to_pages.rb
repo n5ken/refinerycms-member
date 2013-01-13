83963b6060904483e7e936d2331a35946d8cde32
Slava Kravchenko <slava.kravchenko@gmail.com>
2013-01-13 22:15:27 +0300

    fixed pages-related migration

--- db/migrate/20130113101005_add_need_login_to_pages.rb
+++ db/migrate/20130113101005_add_need_login_to_pages.rb
@@ -1,5 +1,5 @@
 class AddNeedLoginToPages < ActiveRecord::Migration
   def change
-    add_column :pages, :need_login, :boolean, :default => 0
+    add_column :refinery_pages, :need_login, :boolean, :default => 0
   end
 end
