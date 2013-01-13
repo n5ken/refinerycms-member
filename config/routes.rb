52cc976d030b17d6bbd5ae89fa25e0a2da80d7f5
Slava Kravchenko <slava.kravchenko@gmail.com>
2013-01-14 00:22:52 +0300

    devise must use Refinery::Member

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Refinery::Core::Engine.routes.prepend do
-  devise_for :members
+  devise_for :members, :class_name => "Refinery::Member"
 
   namespace :admin, :path => 'refinery' do
     resources :members, :except => :show do
