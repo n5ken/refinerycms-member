800dc8f727914e3843fa7ee2da0065d67fc791e4
Slava Kravchenko <slava.kravchenko@gmail.com>
2013-01-14 09:01:36 +0300

    do not run migrations automatically upon "rails g", let developer edit the migration files first

--- lib/generators/refinery/members/members_generator.rb
+++ lib/generators/refinery/members/members_generator.rb
@@ -2,9 +2,6 @@ module Refinery
   class MembersGenerator < Rails::Generators::Base
     source_root File.expand_path('../templates', __FILE__)
 
-    class_option :skip_db,
-                 :desc => "Skip over any database creation, migration or seeding.",
-                 :type => :boolean, :default => false, :aliases => nil, :group => :runtime
     class_option :skip_migrations,
                  :desc => "Skip over installing or running migrations.",
                  :type => :boolean, :default => false, :aliases => nil, :group => :runtime
@@ -18,7 +15,6 @@ module Refinery
     def migrate_database!
       unless self.options[:skip_migrations]
         rake 'refinery_members:install:migrations'
-        rake 'db:create db:migrate' unless self.options[:skip_db]
       end
     end
   end
