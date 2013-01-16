f95e97b8945ae106b4daa5d35d63b2d26fbb6022
Slava Kravchenko <slava.kravchenko@gmail.com>
2013-01-16 16:01:29 +0300

    name members table explicitly

--- app/models/refinery/members/member.rb
+++ app/models/refinery/members/member.rb
@@ -1,6 +1,8 @@
 module Refinery
   module Members
     class Member < Refinery::Core::BaseModel
+      self.table_name = 'refinery_members'
+
       # Include default devise modules. Others available are:
       # :token_authenticatable, :confirmable, :lockable and :timeoutable
       devise :database_authenticatable, :registerable,
